a3be837bb5addfcff71e8dbbff900b327ea6da9a
mafeibiao <1203425460@qq.com>
2017-12-13 14:54:47 +0800

    android消息机制

--- app/src/main/java/com/mafeibiao/testapplication/Messaging/MessageActivity.java
+++ app/src/main/java/com/mafeibiao/testapplication/Messaging/MessageActivity.java
@@ -1,6 +1,7 @@
 package com.mafeibiao.testapplication.Messaging;
 
 import android.os.Bundle;
+import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
 
@@ -18,8 +19,15 @@ public class MessageActivity extends AppCompatActivity {
                 try {
                     //模拟耗时操作
                     Thread.sleep(3* 60 * 1000);
+                    //在子线程中更新UI之前，先准备一个Looper，与主线程相同
+                    if (Looper.myLooper() != null){
+                        Looper.prepare();
+                    }
                     //耗时操作完成之后显示一个通知
                     Toast.makeText(MessageActivity.this,"test",Toast.LENGTH_SHORT).show();
+
+                    //无限循环
+                    Looper.loop();
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
