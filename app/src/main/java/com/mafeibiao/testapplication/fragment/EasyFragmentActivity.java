eeb0f245e620c324f28e7ce307bb727b9c99fc31
mafeibiao <1203425460@qq.com>
2018-01-25 13:28:27 +0800

    android fragment相关知识

--- app/src/main/java/com/mafeibiao/testapplication/fragment/EasyFragmentActivity.java
+++ app/src/main/java/com/mafeibiao/testapplication/fragment/EasyFragmentActivity.java
@@ -2,6 +2,7 @@ package com.mafeibiao.testapplication.fragment;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Display;
 
 import com.mafeibiao.testapplication.R;
 
@@ -11,5 +12,13 @@ public class EasyFragmentActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_easy_fragment);
+        Display display = getWindowManager().getDefaultDisplay();
+        if (display.getWidth() > display.getHeight()) {
+            GoodCarFragment fragment1 = new GoodCarFragment();
+            getSupportFragmentManager().beginTransaction().replace(R.id.main_layout, fragment1).commit();
+        } else {
+            GoodsFragment fragment2 = new GoodsFragment();
+            getSupportFragmentManager().beginTransaction().replace(R.id.main_layout, fragment2).commit();
+        }
     }
 }
