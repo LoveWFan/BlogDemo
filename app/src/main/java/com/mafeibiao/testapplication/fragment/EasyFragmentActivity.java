9c907f1b8b72889f61b86d5c16d2735efb402580
mafeibiao <1203425460@qq.com>
2018-01-25 15:53:29 +0800

    android fragment相关知识

--- app/src/main/java/com/mafeibiao/testapplication/fragment/EasyFragmentActivity.java
+++ app/src/main/java/com/mafeibiao/testapplication/fragment/EasyFragmentActivity.java
@@ -2,15 +2,18 @@ package com.mafeibiao.testapplication.fragment;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Display;
 
 import com.mafeibiao.testapplication.R;
 
 public class EasyFragmentActivity extends AppCompatActivity {
+    private static String TAG= EasyFragmentActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG,"onCreate");
         setContentView(R.layout.activity_easy_fragment);
         Display display = getWindowManager().getDefaultDisplay();
         if (display.getWidth() > display.getHeight()) {
@@ -20,5 +23,42 @@ public class EasyFragmentActivity extends AppCompatActivity {
             GoodsFragment fragment2 = new GoodsFragment();
             getSupportFragmentManager().beginTransaction().replace(R.id.main_layout, fragment2).commit();
         }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(TAG,"onStart");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG,"onResume");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG,"onPause");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG,"onStop");
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG,"onDestroy");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG,"onRestart");
     }
 }
--- app/src/main/java/com/mafeibiao/testapplication/fragment/GoodsFragment.java
+++ app/src/main/java/com/mafeibiao/testapplication/fragment/GoodsFragment.java
@@ -1,8 +1,10 @@
 package com.mafeibiao.testapplication.fragment;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,16 +16,80 @@ import com.mafeibiao.testapplication.R;
  * Created by Chenyc on 15/7/1.
  */
 public class GoodsFragment extends Fragment {
-
+    private static String TAG= GoodsFragment.class.getSimpleName();
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        Log.d(TAG,"onAttach");
+    }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        Log.d(TAG,"onCreateView");
         View view = inflater.inflate(R.layout.fragment_goods, null);
 
         return view;
     }
-    
 
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Log.d(TAG,"onViewCreated");
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        Log.d(TAG,"onActivityCreated");
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d(TAG,"onCreate");
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(TAG,"onStart");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG,"onResume");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG,"onPause");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG,"onStop");
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        Log.d(TAG,"onDestroyView");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG,"onDestroy");
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        Log.d(TAG,"onDetach");
+    }
 
 }
